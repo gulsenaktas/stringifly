e31159a2a70d9e8428794fc439b473e4899bdf1a
gulsenaktas <aaktasgulsen@gmail.com>
2021-01-24 21:15:26 +0300

    1.0 first structure

---
+++ scripts/app.js
@@ -0,0 +1,50 @@
+const chatList = document.querySelector(".chat-list");
+const newChatForm = document.querySelector(".new-chat");
+const newNameForm = document.querySelector(".new-name");
+const updateMssg = document.querySelector(".update-message");
+const rooms = document.querySelector(".chat-rooms")
+const buttons = document.querySelectorAll(".chat-rooms .btn")
+const chatWindow = document.querySelector(".chat-window")
+
+newChatForm.addEventListener("submit", e=>{
+    e.preventDefault();
+    const message = newChatForm.message.value.trim();
+    chatroom.addChat(message)
+    .then(()=> newChatForm.reset())
+    .catch(err=> console.log(err));
+});
+
+newNameForm.addEventListener("submit", e => {
+    e.preventDefault();
+    const newName = newNameForm.name.value.trim();
+    chatroom.updateName(newName);
+
+    newNameForm.reset();
+
+    updateMssg.innerText = `Your name was update to ${newName}`
+    setTimeout(()=> updateMssg.innerText = "", 3000)
+});
+
+rooms.addEventListener("click", e => {
+    if(e.target.classList.contains("btn")){
+        chatUI.clear();
+        chatroom.updateRoom(e.target.getAttribute("id"));
+        chatroom.getChats(chat => chatUI.render(chat));
+    }
+});
+
+rooms.addEventListener("click",e=>{
+    if(e.target.classList.contains("btn")){
+        buttons.forEach(element=>{
+           element.classList.remove("btn-on")
+        })
+        e.target.classList.add("btn-on")        
+    }
+})
+
+const username = localStorage.getItem("username") ? localStorage.getItem("username") : "anonymous";
+
+const chatUI = new ChatUI(chatList)
+const chatroom = new Chatroom("music", username);
+
+chatroom.getChats(data => chatUI.render(data));
